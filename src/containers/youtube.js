5f30ee915b3ab5eed91355d37a856752115fd3a1
Jared VanderHook <jared.vanderhook@gmail.com>
2015-12-09 16:22:41 -0600

    removed semicolons

--- src/containers/youtube.js
+++ src/containers/youtube.js
@@ -1,20 +1,20 @@
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
-import Youtube from '../components/Youtube';
-import * as actions from '../modules/youtube';
+import Youtube from '../components/Youtube'
+import * as actions from '../modules/youtube'
 
 // This is where we define computed fields (reselect module) or make other changes.
 // Which part of the Redux global state does our component want to receive as props?
 function mapStateToProps(state) {
-  return state.youtube;
+  return state.youtube
 }
 
 // Which action creators does it want to receive by props?
 // This gets merged into props too.
 // Not sure why it needs to happen here.
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators(actions, dispatch);
+  return bindActionCreators(actions, dispatch)
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Youtube);
+export default connect(mapStateToProps, mapDispatchToProps)(Youtube)
