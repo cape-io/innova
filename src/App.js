27b00b0aed452e3c70dd334faed9f3550f3407ac
Kai Curry <kai@cape.io>
2015-12-15 09:46:30 -0600

    Move resolveUrl up the component chain

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react'
-import Youtube from './containers/Youtube'
-import Soundcloud from './containers/soundcloud'
+import React, { Component, PropTypes } from 'react'
 
-export class App extends Component {
+import { connect } from 'react-redux'
+
+// This is where we define computed fields (reselect module) or make other changes.
+// Which part of the Redux global state does our component want to receive as props?
+function mapStateToProps(state) {
+  const { frontPage, album } = state
+  return { frontPage, album }
+}
+
+class App extends Component {
   render() {
+    const { album, frontPage } = this.props
     return (
       <div className="container">
-        <Youtube />
-        <Soundcloud />
+        { frontPage && <Youtube /> }
+        { album && album.soundcloudTrack &&
+          <Soundcloud resolveUrl={album.soundcloudTrack} />
+        }
       </div>
     )
   }
 }
+App.propTypes = {
+  album: PropTypes.object.isRequired,
+  frontPage: PropTypes.bool.isRequired
+}
+export default connect(mapStateToProps)(App)
--- src/containers/soundcloud.js
+++ src/containers/soundcloud.js
@@ -5,11 +5,12 @@ import PlayerWidget from '../components/PlayerWidget'
 
 class soundcloud extends Component {
   render() {
+    const { resolveUrl } = this.props
     return (
       <div>
         <SoundPlayerContainer
           clientId="416cb3f3e2a04d5ec901dba6f94aaa49"
-          resolveUrl="https://soundcloud.com/innovadotmu/06-sally-ann"
+          resolveUrl={resolveUrl}
         >
           <PlayerWidget />
         </SoundPlayerContainer>
