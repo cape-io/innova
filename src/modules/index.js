37f67888a1bb6fe8dc82413d844170050aa4e014
Kai Curry <kai@cape.io>
2015-12-15 11:27:01 -0600

    style

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,8 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
 
-import youtube from './youtube';
+import youtube from './youtube'
 
 // Define what part of the store the reducer is responsible for.
 export default combineReducers({
   youtube,
-});
+})
